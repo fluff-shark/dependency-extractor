a5f83ef01b0e57a1283ec7f8a1863b3f36265296
Dave Bemiller <116469934+fluff-shark@users.noreply.github.com>
2022-10-23 02:18:12 -0400

    Replaced glob with globwalk.

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,15 +1,15 @@
 use std::path::PathBuf;
 
-use glob::glob;
+use globwalk::GlobWalkerBuilder;
 
 pub fn find_ts_filenames(base_path: String) -> Vec<PathBuf> {
-    let full_glob = format!("{}/**/*jsx", base_path);
+    let walker = match GlobWalkerBuilder::from_patterns(&base_path, &["**/*.{js,jsx,ts,tsx}"]).build() {
+        Ok(the_walker) => the_walker,
+        Err(e) => panic!("Glob error: {}", e),
+    };
 
-    return match glob(&full_glob) {
-        Ok(paths) => paths.map(|glob_result| match glob_result {
-            Ok(path_buf) => path_buf,
-            Err(e) => panic!("Error resolving paths against glob: {}", e),
-        }),
-        Err(pattern_error) => panic!("Error compiling glob {}: {}", full_glob, pattern_error),
-    }.collect();
+    return walker.map(|m| match m {
+        Ok(dir_entry) => dir_entry.into_path(),
+        Err(e) => panic!("Error searching {}: {}", base_path, e),
+    }).collect();
 }
