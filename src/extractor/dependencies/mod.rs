c0ee954fe9435eb76d5d80a98e95983811788c9c
Dave Bemiller <116469934+fluff-shark@users.noreply.github.com>
2022-10-30 16:02:45 -0400

    Fixed compile error

--- src/extractor/dependencies/mod.rs
+++ src/extractor/dependencies/mod.rs
@@ -5,10 +5,7 @@ mod parse;
 
 pub fn log_dependencies(input: &Path, output_path: &Path) {
      match parse::parse_module(input) {
-        Ok(module) => {
-            let dependencies = 
-            output::write_dependencies(output_path, module)
-        },
+        Ok(module) => output::write_dependencies(output_path, module),
         Err(e) => {
             e.io_error.iter().for_each(|err| eprintln!("failed to read {}: {:?}", input.display(), err));
         }
